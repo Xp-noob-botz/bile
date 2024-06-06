9607877fa64a0eb7c4e8fd56dd29bf880f145eca
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-07 00:43:02 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -9,7 +9,6 @@ from helper.database import AshutoshGoswami24
 from asyncio import sleep
 from PIL import Image
 import os, time
-import random
 from config import Config
 
 @Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
@@ -132,15 +131,13 @@ async def doc(bot, update):
  
     if (media.thumbs or c_thumb):
         if c_thumb:
-            ph_path = await bot.download_media(c_thumb)
-            width, height, ph_path = await fix_thumb(ph_path)
+            ph_path = await bot.download_media(c_thumb) 
         else:
-            try:
-                ph_path_ = await take_screen_shot(file_path, os.path.dirname(os.path.abspath(file_path)), random.randint(0, duration - 1))
-                width, height, ph_path = await fix_thumb(ph_path_)
-            except Exception as e:
-                ph_path = None
-                print(e)
+            ph_path = await bot.download_media(media.thumbs[0].file_id)
+        Image.open(ph_path).convert("RGB").save(ph_path)
+        img = Image.open(ph_path)
+        img.resize((320, 320))
+        img.save(ph_path, "JPEG")
 
     await ms.edit("𝗣𝗮𝗻𝗱𝗮𝗪𝗲𝗽 𝗧𝗿𝘆𝗶𝗻𝗴 𝗧𝗼 𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴")
     type = update.data.split("_")[1]
@@ -157,12 +154,10 @@ async def doc(bot, update):
             await bot.send_video(
                 update.message.chat.id,
                 video=file_path,
-                    caption=caption,
-                    thumb=ph_path,
-                    width=width,
-                    height=height,
-                    duration=duration,
-                    progress=progress_for_pyrogram,
+                caption=caption,
+                thumb=ph_path,
+                duration=duration,
+                progress=progress_for_pyrogram,
                 progress_args=("𝗨𝗽𝗹𝗼𝗮𝗱 𝗦𝘁𝗮𝗿𝘁𝗲𝗱｡｡｡｡", ms, time.time()))
         elif type == "audio": 
             await bot.send_audio(
